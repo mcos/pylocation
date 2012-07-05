0d80463e87ce9058504d7ef64048802ea68fe634
Mark Costello <mark@pixability.com>
2012-07-05 16:04:54 -0400

    Commit of working prototype

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-setup(name='pylocate',
+setup(name='pylocation',
       version='0.1',
       description='Python Wrapper Library for Geolocating IP Addresses',
       author='Mark Costello',
       author_email='me@markcostello.net',
-      url='https://github.com/mcos/pylocate',
-      license="MIT License"
-      # install_requires=['requests','simplejson']
-      # packages=['zencoder']
+      url='https://github.com/mcos/pylocation',
+      license="MIT License",
+      install_requires=['requests'],
+      packages=['pylocation']
      )
